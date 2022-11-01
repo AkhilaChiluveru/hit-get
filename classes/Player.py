482afb7a182547690c62389090ecb90dff1e95a7
AkhilaChiluveru <akhila.chiluveru@pdx.edu>
2022-10-31 17:32:21 -0700

    Movement of player added in class files

---
+++ classes/Player.py
@@ -0,0 +1,7 @@
+import pygame
+window_width,window_height=1280,720
+class Player(pygame.sprite.Sprite):
+    def __init__(self,groups):
+        super().__init__(groups)
+        self.image=pygame.image.load('./graphics/ship.png').convert_alpha()
+        self.rect=self.image.get_rect(center=(window_width/2,window_height/2))
