eea5723a6a2f8a81036475c9e69a73e5c822d069
AkhilaChiluveru <akhila.chiluveru@pdx.edu>
2022-11-09 15:59:22 -0800

    refactoring names

--- classes/Bullet.py
+++ classes/Bullet.py
@@ -2,6 +2,6 @@ import pygame
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,position,groups):
         super().__init__(groups)
-        self.image=pygame.image.load('./graphics/laser.png').convert_alpha()
+        self.image=pygame.image.load('./graphics/bullets.png').convert_alpha()
         self.rect=self.image.get_rect(midbottom=position)
 
--- classes/Targets.py
+++ classes/Targets.py
@@ -3,6 +3,6 @@ import pygame
 class Targets(pygame.sprite.Sprite):
     def __init__(self,positon,groups):
         super().__init__(groups)
-        self.image=pygame.image.load('./graphics/meteor.png').convert_alpha()
+        self.image=pygame.image.load('./graphics/targets.png').convert_alpha()
         self.rect=self.image.get_rect(center=positon)
        
