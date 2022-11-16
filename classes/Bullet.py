ac6ca304e09467964e884e97a915fd9485d40075
vkarinje <vkarinje@pdx.edu>
2022-11-16 15:19:39 -0800

    correcting  my file path

--- classes/Bullet.py
+++ classes/Bullet.py
@@ -2,7 +2,7 @@ import pygame
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,position,groups):
         super().__init__(groups)
-        self.image=pygame.image.load('../graphics/bullets.png').convert_alpha()
+        self.image=pygame.image.load('./graphics/bullets.png').convert_alpha()
         self.rect=self.image.get_rect(midbottom=position)
 
         self.pos=pygame.math.Vector2(self.rect.topleft)
