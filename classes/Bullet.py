af89b1abdd38cb5dd2340907637aa78f4f39f127
Kajal Patil <kajal@pdx.edu>
2022-11-16 19:34:06 -0800

    update score on every hit

--- classes/Bullet.py
+++ classes/Bullet.py
@@ -1,4 +1,5 @@
 import pygame
+import Score
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,position,groups):
         super().__init__(groups)
@@ -7,18 +8,18 @@ class Bullet(pygame.sprite.Sprite):
 
         self.pos=pygame.math.Vector2(self.rect.topleft)
         self.direction=pygame.math.Vector2(0,-1) #-1 for upwards 0 for left/ right
-        self.speed=600
+        self.speed=1000
         
-    def hit_target(self,target_group,scoreCount):
+    def hit_target(self,target_group):
         if pygame.sprite.spritecollide(self,target_group,True):
-            scoreCount+=1
+            Score.varScore+=1
             self.kill()
 
-    def update(self,dt,target_group,scoreCount):
+    def update(self,dt,target_group):
         self.pos += self.direction* self.speed * dt 
         self.rect.topleft= (round(self.pos.x),round(self.pos.y))
 
         if self.rect.bottom < 0:
             self.kill()
         
-        self.hit_target(target_group,scoreCount)
+        self.hit_target(target_group)
