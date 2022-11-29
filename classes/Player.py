2d0038e85f7298d4581094a32e80efc3c75736b5
kajal8879 <kajal@pdx.edu>
2022-11-28 16:42:49 -0800

    adding a background music to the game

--- classes/Player.py
+++ classes/Player.py
@@ -7,11 +7,11 @@ class Player(pygame.sprite.Sprite):
     
     def __init__(self,groups):
         super().__init__(groups)
-        self.image=pygame.image.load('./graphics/player.png').convert_alpha()
+        self.image=pygame.image.load('../graphics/player.png').convert_alpha()
         self.rect=self.image.get_rect(center=(window_width/2,window_height/2))
         self.can_shoot = True
         self.shoot_time= None
-        self.bullet_sound=pygame.mixer.Sound('./sounds/laser.ogg')
+        self.bullet_sound=pygame.mixer.Sound('../sounds/laser.ogg')
 
     def bullet_timer(self):
         if not self.can_shoot:
--- classes/Score.py
+++ classes/Score.py
@@ -4,7 +4,7 @@ import pygame
 class Score():
 
 	def __init__(self):
-		self.font = pygame.font.Font('./graphics/subatomic.ttf',30)
+		self.font = pygame.font.Font('../graphics/subatomic.ttf',30)
 		global varScore
 		varScore=0
 
