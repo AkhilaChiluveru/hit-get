07db5500ba6e18324eb3f9fd279564cfa2a1e5c3
kajal8879 <kajal@pdx.edu>
2022-11-29 23:15:41 -0800

    Show game over message on player target collision

--- classes/Targets.py
+++ classes/Targets.py
@@ -10,11 +10,12 @@ class Targets(pygame.sprite.Sprite):
         self.pos=pygame.math.Vector2(self.rect.topleft)
         self.direction=pygame.math.Vector2(uniform(-0.5,0.5),1)
         self.speed=randint(200,300)
-
+    
     #updating the position for the target movement to be a bit more random
     def update(self,dt,WINDOW_HEIGHT):
         self.pos+=self.direction * self.speed * dt
         self.rect.topleft=(round(self.pos.x),round(self.pos.y))
+    
         if self.rect.top > WINDOW_HEIGHT:
             self.kill()
 
