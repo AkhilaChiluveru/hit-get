125a8c785172509d5daaee6d926e4f52ef5b8262
kajal8879 <kajal@pdx.edu>
2022-12-01 11:41:20 -0800

    test cases addition

--- classes/BulletTest.py
+++ classes/BulletTest.py
@@ -27,6 +27,17 @@ class Bullet_test(unittest.TestCase):
         bullet.hit_target(target_group)
         self.assertEqual(prev_score+1, Score.varScore)
 
+    @patch('pygame.sprite.spritecollide')
+    def test_score_increment_onNoCollision(self,mock_sprite_collision):
+        mock_sprite_collision.return_value= False
+        target_group=pygame.sprite.Group()
+        bullet_group= pygame.sprite.Group()
+        pos=pygame.mouse.get_pos()
+        bullet = Bullet(pos,bullet_group)
+        Score.varScore=5
+        prev_score=5
+        bullet.hit_target(target_group)
+        self.assertEqual(prev_score, Score.varScore)
 
 if __name__=='__main__':
         unittest.main()
